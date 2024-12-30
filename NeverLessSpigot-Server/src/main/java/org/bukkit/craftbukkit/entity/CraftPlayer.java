d388108278b63ce5a69fc7c915fdad0e9b941d00
Ympax14 <ympax14@gmail.com>
2024-12-30 14:14:04 +0100

    Remove unused import

--- NeverLessSpigot-Server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ NeverLessSpigot-Server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -7,7 +7,6 @@ import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
